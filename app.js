0b362df45e3d7a9a359317031bc4fa1d4e035392
bassem eloraby <BASEM12683@GMAIL.COM>
2022-05-13 14:47:02 +0300

    morgan

--- app.js
+++ app.js
@@ -2,14 +2,19 @@ const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
 
-const connectDB = require('./config/db')
+const connectDB = require('./config/db');
 
 //load config
 dotenv.config({ path: './config/config.env' });
 
-connectDB()
+connectDB();
 
 const app = express();
+
+// Logging
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
 const PORT = process.env.PORT || 3000;
 
