cd98cfe45285158160942131065f6fd5d912f83f
bassem eloraby <BASEM12683@GMAIL.COM>
2022-05-13 14:43:46 +0300

    config

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+
+const connectDB = require('./config/db')
+
+//load config
+dotenv.config({ path: './config/config.env' });
+
+connectDB()
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(
+  PORT,
+  console.log(`server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
